bf353a5303a099430955d94899c4e79926ce4d9f
fufuU16 <151064644+fufuU16@users.noreply.github.com>
2024-06-03 05:01:29 +0800

    Update AdminHelpdeskEdit.php

--- AdminHelpdeskEdit.php
+++ AdminHelpdeskEdit.php
@@ -1,6 +1,23 @@
 <?php
-session_start();
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
+session_start();
+function handlePhpError($errno, $errstr, $errfile, $errline) {
+    echo "<script type='text/javascript'>
+            alert('Error: $errstr in $errfile on line $errline');
+          </script>";
+}
+
+set_error_handler('handlePhpError');
+
+$error_log_file = __DIR__ . '/error_log.txt';
+
+function log_error($message) {
+    global $error_log_file;
+    error_log($message . "\n", 3, $error_log_file);
+}
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
