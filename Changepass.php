5b57aafc267abda71086517cfab296342ebd1a63
fufuU16 <151064644+fufuU16@users.noreply.github.com>
2024-06-03 03:45:45 +0800

    Update Changepass.php

--- Changepass.php
+++ Changepass.php
@@ -1,11 +1,12 @@
 
     <?php
+session_start();
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-            session_start();
+           
 
     include 'db.php';
 function handlePhpError($errno, $errstr, $errfile, $errline) {
