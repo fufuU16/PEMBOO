3d4687931830688ee4a84adb0710ed04b2a99925
fufuU16 <151064644+fufuU16@users.noreply.github.com>
2024-06-03 05:28:22 +0800

    Update Admincreate.php

--- Admincreate.php
+++ Admincreate.php
@@ -1,7 +1,24 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Include database connection
 include 'db.php';
+function handlePhpError($errno, $errstr, $errfile, $errline) {
+    echo "<script type='text/javascript'>
+            alert('Error: $errstr in $errfile on line $errline');
+          </script>";
+}
 
+set_error_handler('handlePhpError');
+
+$error_log_file = __DIR__ . '/error_log.txt';
+
+function log_error($message) {
+    global $error_log_file;
+    error_log($message . "\n", 3, $error_log_file);
+}
 // Function to hash password
 function hashPassword($password) {
     return password_hash($password, PASSWORD_BCRYPT);
