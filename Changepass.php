4f6eddcdac66a756a32b373d521c7e0dfa67e139
fufuU16 <151064644+fufuU16@users.noreply.github.com>
2024-06-03 03:18:34 +0800

    Update Changepass.php

--- Changepass.php
+++ Changepass.php
@@ -1,113 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="Signup.css">
-    <title>Change Password</title>
-    <link rel="icon" type="image/png" href="picture\icon.png">
-
-    <style>
-        /* Adjusted CSS for dropdown */
-        .dropdown {
-            position: relative;
-            display: inline-block;
-            float: right; /* Float the dropdown to the right */
-        }
-
-        .dropdown-content {
-            display: none;
-            position: absolute;
-            min-width: 160px;
-            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-            z-index: 1;
-            right: 0; /* Align dropdown content to the right */
-        }
-
-        .dropdown-content a {
-            color: black;
-            padding: 12px 16px;
-            text-decoration: none;
-            display: block;
-        }
-
-        .dropdown-content a:hover {
-            background-color: #f1f1f1;
-        }
-
-        .dropdown:hover .dropdown-content {
-            display: block;
-        }
-    </style>
-</head>
-<body>
-    <div class="top-image">
-        <img src="picture/Imagetop.png" alt="Header Image">
-    </div>
-    <div class="navbar">
-        <a href="index.php">Home</a>
-        <a href="BarangayUp.php">BARANGAY UPDATES</a>
-        <a href="Entertainment.php">ENTERTAINMENT</a>
-        <a href="Helpdesk.php">HELP DESK</a>
-        <?php
-            session_start();
-
-            if(isset($_SESSION['user_id'])) {
-                // User is logged in, display dropdown with Logout and Change Password
-                echo '
-                <div class="dropdown">
-                    <a href="#" class="login">Account</a>
-                    <div class="dropdown-content">
-                        <a href="changepass.php">Change Password</a>
-                        <a href="logout.php">Logout</a>
-                    </div>
-                </div>';
-            } else {
-                // User is not logged in, display Login
-                echo '<a href="login.php" class="login">Login</a>';
-            }
-        ?>
-    </div>
-
-    <div class="Login">
-        <div class="login-container">
-            <div class="half-left">
-                <div class="centered">
-                </div>
-            </div>
-            <div class="half-right">
-                <div class="centered">
-                </div>
-            </div>
-        </div>
-
-        <div class="overlay-container">
-            <div class="centered">
-                <h2></h2>
-            </div>
-            <div class="overlay-left">
-            </div>
-            <div class="overlay-right">
-                <h2>Change Password</h2>
-                <form id="changePasswordForm" method="post" action="changepass.php">
-                    <?php
-                    if (isset($_SESSION['email'])) {
-                        echo '<input type="text" name="email" placeholder="Email" value="' . htmlspecialchars($_SESSION['email']) . '" readonly><br><br>';
-                    } else {
-                        echo '<script>alert("Session expired. Please log in again."); window.location.href="login.php";</script>';
-                    }
-                    ?>
-                    <input type="password" name="oldPassword" placeholder="Old Password" required><br><br>
-                    <input type="password" name="newPassword" placeholder="New Password" required><br><br>
-                    <button type="submit" name="submit">Change Password</button>
-                </form>
-            </div>
-        </div>
-    </div>
 
     <?php
-    include 'db.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
+
+            session_start();
+
+    include 'db.php';
+function handlePhpError($errno, $errstr, $errfile, $errline) {
+    echo "<script type='text/javascript'>
+            alert('Error: $errstr in $errfile on line $errline');
+          </script>";
+}
+
+set_error_handler('handlePhpError');
+
+$error_log_file = __DIR__ . '/error_log.txt';
+
+function log_error($message) {
+    global $error_log_file;
+    error_log($message . "\n", 3, $error_log_file);
+}
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $email = $_POST['email'];
         $oldPassword = $_POST['oldPassword'];
@@ -173,6 +87,114 @@
         $conn->close();
     }
     ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Signup.css">
+    <title>Change Password</title>
+    <link rel="icon" type="image/png" href="picture\icon.png">
+
+    <style>
+        /* Adjusted CSS for dropdown */
+        .dropdown {
+            position: relative;
+            display: inline-block;
+            float: right; /* Float the dropdown to the right */
+        }
+
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+            z-index: 1;
+            right: 0; /* Align dropdown content to the right */
+        }
+
+        .dropdown-content a {
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+        }
+
+        .dropdown-content a:hover {
+            background-color: #f1f1f1;
+        }
+
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
+    </style>
+</head>
+<body>
+    <div class="top-image">
+        <img src="picture/Imagetop.png" alt="Header Image">
+    </div>
+    <div class="navbar">
+        <a href="index.php">Home</a>
+        <a href="BarangayUp.php">BARANGAY UPDATES</a>
+        <a href="Entertainment.php">ENTERTAINMENT</a>
+        <a href="Helpdesk.php">HELP DESK</a>
+        <?php
+
+            if(isset($_SESSION['user_id'])) {
+                // User is logged in, display dropdown with Logout and Change Password
+                echo '
+                <div class="dropdown">
+                    <a href="#" class="login">Account</a>
+                    <div class="dropdown-content">
+                        <a href="changepass.php">Change Password</a>
+                        <a href="logout.php">Logout</a>
+                    </div>
+                </div>';
+            } else {
+                // User is not logged in, display Login
+                echo '<a href="login.php" class="login">Login</a>';
+            }
+        ?>
+    </div>
+
+    <div class="Login">
+        <div class="login-container">
+            <div class="half-left">
+                <div class="centered">
+                </div>
+            </div>
+            <div class="half-right">
+                <div class="centered">
+                </div>
+            </div>
+        </div>
+
+        <div class="overlay-container">
+            <div class="centered">
+                <h2></h2>
+            </div>
+            <div class="overlay-left">
+            </div>
+            <div class="overlay-right">
+                <h2>Change Password</h2>
+                <form id="changePasswordForm" method="post" action="changepass.php">
+                    <?php
+                    if (isset($_SESSION['email'])) {
+                        echo '<input type="text" name="email" placeholder="Email" value="' . htmlspecialchars($_SESSION['email']) . '" readonly><br><br>';
+                    } else {
+                        echo '<script>alert("Session expired. Please log in again."); window.location.href="login.php";</script>';
+                    }
+                    ?>
+                    <input type="password" name="oldPassword" placeholder="Old Password" required><br><br>
+                    <input type="password" name="newPassword" placeholder="New Password" required><br><br>
+                    <button type="submit" name="submit">Change Password</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
 
     <script src="Login.js"></script>
 </body>
