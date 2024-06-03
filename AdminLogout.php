2e229b69ce955f75feccd00a7d2171001a707998
fufuU16 <151064644+fufuU16@users.noreply.github.com>
2024-06-03 13:20:43 +0800

    Update AdminLogout.php

--- AdminLogout.php
+++ AdminLogout.php
@@ -6,15 +6,15 @@ include 'db.php';
 session_start();
 
 // Check if the user is logged in
-if (isset($_SESSION['email'])) {
+if (isset($_SESSION['id'])) {
     // Retrieve user ID and email from session
-    $user_id = $_SESSION['user_id'];
+    $admin_id = $_SESSION['id'];
     $email = $_SESSION['email'];
 
     // Insert log entry for logout
     $log_description = "Admin logged out with email: $email";
     $insert_log_stmt = $conn->prepare("INSERT INTO adminLogs (admin_id, log_type, log_description) VALUES (?, 'Admin Logout', ?)");
-    $insert_log_stmt->bind_param("is", $user_id, $log_description);
+    $insert_log_stmt->bind_param("is", $admin_id, $log_description);
     $insert_log_stmt->execute();
     $insert_log_stmt->close();
 }
