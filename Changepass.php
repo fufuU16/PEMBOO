1435046e842ecaee1719416efb9eb64b1214c8c0
fufuU16 <151064644+fufuU16@users.noreply.github.com>
2024-06-03 15:13:25 +0800

    Update Changepass.php

--- Changepass.php
+++ Changepass.php
@@ -1,9 +1,28 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start();
 
 
 include 'db.php';
 
+
+function handlePhpError($errno, $errstr, $errfile, $errline) {
+    echo "<script type='text/javascript'>
+            alert('Error: $errstr in $errfile on line $errline');
+          </script>";
+}
+
+set_error_handler('handlePhpError');
+
+$error_log_file = __DIR__ . '/error_log.txt';
+
+function log_error($message) {
+    global $error_log_file;
+    error_log($message . "\n", 3, $error_log_file);
+}
 
 if (!isset($_SESSION['email']) || empty($_SESSION['email'])) {
     echo '<script>alert("Session expired. Please log in again."); window.location.href="login.php";</script>';
