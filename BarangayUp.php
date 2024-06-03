acc6071188ff4afdf6f05e7e7f52930a6b3873e1
fufuU16 <151064644+fufuU16@users.noreply.github.com>
2024-06-03 14:51:55 +0800

    Update BarangayUp.php

--- BarangayUp.php
+++ BarangayUp.php
@@ -1,11 +1,10 @@
-
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+session_start();
 
-      session_start();
 function handlePhpError($errno, $errstr, $errfile, $errline) {
     echo "<script type='text/javascript'>
             alert('Error: $errstr in $errfile on line $errline');
@@ -20,7 +19,6 @@ function log_error($message) {
     global $error_log_file;
     error_log($message . "\n", 3, $error_log_file);
 }
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -32,94 +30,88 @@ function log_error($message) {
 
     <title>Barangay Updates</title>
     <style>
-/* Adjusted CSS for dropdown */
-.dropdown {
-    position: relative;
-    display: inline-block;
-    float: right; /* Float the dropdown to the right */
-}
+        /* Adjusted CSS for dropdown */
+        .dropdown {
+            position: relative;
+            display: inline-block;
+            float: right; /* Float the dropdown to the right */
+        }
 
-.dropdown-content {
-    display: none;
-    position: absolute;
-    min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 1;
-    right: 0; /* Align dropdown content to the right */
-}
+        .dropdown-content {
+            display: none;
+            position: absolute;
+            min-width: 160px;
+            box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+            z-index: 1;
+            right: 0; /* Align dropdown content to the right */
+        }
 
-.dropdown-content a {
-    color: black;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-}
+        .dropdown-content a {
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
+        }
 
-.dropdown-content a:hover {
-    background-color: #f1f1f1;
-}
+        .dropdown-content a:hover {
+            background-color: #f1f1f1;
+        }
 
-.dropdown:hover .dropdown-content {
-    display: block;
-}
-
+        .dropdown:hover .dropdown-content {
+            display: block;
+        }
     </style>
 </head>
 <body>
     <div class="top-image">
         <img src="picture/Imagetop.png" alt="Header Image">
-    
     </div>
     <div class="navbar">
-        <a href="index.php" >Home</a>
-        <a href="BarangayUp.php"class="home">BARANGAY UPDATES</a>
+        <a href="index.php">Home</a>
+        <a href="BarangayUp.php" class="home">BARANGAY UPDATES</a>
         <a href="Entertainment.php">ENTERTAINMENT</a>
         <a href="Helpdesk.php">HELP DESK</a>
         <?php
-          
-
-if(isset($_SESSION['user_id'])) {
-    // User is logged in, display dropdown with Logout and Change Password
-    echo '
-    <div class="dropdown">
-    <a href="#" class="login">Account</a>
-        <div class="dropdown-content">
-            <a href="changepass.php">Change Password</a>
-            <a href="notification.php">Notifications</a>
-
-            <a href="logout.php">Logout</a>
-        </div>
-    </div>';
-} else {
-    // User is not logged in, display Login
-    echo '<a href="login.php" class="login">Login</a>';
-}
-?>
+        if(isset($_SESSION['user_id'])) {
+            // User is logged in, display dropdown with Logout and Change Password
+            echo '
+            <div class="dropdown">
+                <a href="#" class="login">Account</a>
+                <div class="dropdown-content">
+                    <a href="changepass.php">Change Password</a>
+                    <a href="notification.php">Notifications</a>
+                    <a href="logout.php">Logout</a>
+                </div>
+            </div>';
+        } else {
+            // User is not logged in, display Login
+            echo '<a href="login.php" class="login">Login</a>';
+        }
+        ?>
     </div>
-   
+
     <div class="titlee">
         <span class="part1">BARANGAY </span>
         <span class="part2">UPDATES</span>
     </div>
-   <div class="show">
+    <div class="show">
         <div class="image1">
-        <img src="picture/slide1.png" alt="Image">
+            <img src="picture/slide1.png" alt="Image">
         </div>
         <div class="description">
             <h2>Pembo Elementary School</h2>
             <p>Barangay Pembo's mission is innovative transformation and global change through adopting modernization and open sourcing, sustainably holistic, morally self-replicating. Highest of good of all solutions founded on comprehensive and modifiable community, models duplicated globally that include sustainable development goals for infrastructure, food, education and arts, peace and order disaster resilience, economics and fulfilled living.</p>
         </div>
-   </div>
-   <div class="othershow">
-   <div class="image2">
-        <img src="picture/slide2.png" alt="Image">
+    </div>
+    <div class="othershow">
+        <div class="image2">
+            <img src="picture/slide2.png" alt="Image">
         </div>
         <div class="image3">
-        <img src="picture/slide3.png" alt="Image">
+            <img src="picture/slide3.png" alt="Image">
         </div>
-   </div>
+    </div>
 
-   <script src="BarangayUp.js"></script>
-
+    <script src="BarangayUp.js"></script>
 </body>
 </html>
